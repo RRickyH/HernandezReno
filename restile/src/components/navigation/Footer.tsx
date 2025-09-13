7c5ac7e08b81a229366e495069309894551a30a4
Roberto Hernandez <ricky4nhernandez@gmail.com>
2025-09-12 23:49:39 -0400

    Add an initial navbar
     - General impromevents to colours

--- restile/src/components/navigation/Footer.tsx
+++ restile/src/components/navigation/Footer.tsx
@@ -1,19 +1,13 @@
-import { NavLink } from "react-router-dom";
-
 const Footer = () => {
   return (
     <footer>
-      <div className="bg-gray-800 items-center justify-center w-full gap-8">
-        <h3 className={"text-lg font-semibold justify-center text-center"}>
-          Hernandez Renovations
+      <div className="bg-gray-900 items-center justify-center w-full gap-8 p-2">
+        <h3 className={"text-lg font-semibold justify-center text-center text-gray-50"}>
+          Hernández Renovations
         </h3>
-        <p className="text-sm justify-center text-center">
+        <p className="text-sm justify-center text-center text-gray-400">
           &copy; {new Date().getFullYear()} All rights reserved.
         </p>
-        <div className={"flex justify-center gap-20"}>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-        </div>
       </div>
     </footer>
   );
